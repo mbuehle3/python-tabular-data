16e7ed0704b9a7b1607e80b05b795b39f94f51ce
mbuehle3 <mbuehle3@gmail.com>
2021-04-25 14:17:24 -0500

    added docstrings

--- RegPlot.py
+++ RegPlot.py
@@ -8,12 +8,39 @@ import sys
 
 def read_data(data):
     '''
+    DESCRIPTION
+    ------
+    A simple function to read in a csv file convert it to a pandas dataframe and store it as an object.
+    ------
+    INPUT
+    ------
+    data = a str with the path to a csv file you want to input
+    ------
+    OUTPUT
+    ------
+    A stored pandas dataframe
     '''
     df = pd.read_csv(data)
 
     return df
 
 def plot_variables(df, column1, column2, output = 'output'):
+    '''
+    DESCRIPTION
+    ------
+    This function creates a scatter plot for two variables
+    ------
+    INPUT
+    ------
+    df = a pandas dataframe that can be read in from a csv file using the RegPlot.read_data('csvfile') argument
+    column1 = a column of int that you want to plot
+    column2 = a column of int that you want to plot and regress against column1
+    output = a str containing the desired name for an output file, without the file extension. Default = output
+    ------
+    OUTPUT
+    ------
+    A .png file containing a scatter plot of the data    
+    '''
 
     x = df[column1]
     y = df[column2]
@@ -25,6 +52,20 @@ def plot_variables(df, column1, column2, output = 'output'):
 
 def plot_reg(df, column1, column2, output = 'output'):
     '''
+    DESCRIPTION
+    ------
+    This function creates a regression plot for any two variables
+    ------
+    INPUT
+    ------
+    df = a pandas dataframe that can be read in from a csv file using the RegPlot.read_data('csvfile') argument
+    column1 = a column of int that you want to plot
+    column2 = a column of int that you want to plot and regress against column1
+    output = a str containing the desired name for an output file, without the file extension. Default = output
+    ------
+    OUTPUT
+    ------
+    A .png file containing a scatter plot of the data with a regression line through the plot
     '''
     x = df[column1]
     y = df[column2]
@@ -41,6 +82,21 @@ def plot_reg(df, column1, column2, output = 'output'):
 
 def plot_all(df, column1, column2, groups):
     '''
+    ------
+    DESCRIPTION
+    ------
+    Use this function to create a plot of variable data and regression of two variables, sorting by categories in a column.
+    ------
+    INPUT
+    ------
+    df = a pandas dataframe that can be read in from a csv file using the RegPlot.read_data('csvfile') argument
+    column1 = a column of int that you want to plot
+    column2 = a column of int that you want to plot and regress against column1
+    groups = categorical data of type str that will be used to create the different plots
+    ------
+    OUTPUT
+    ------
+    For each variable of categorical data there will be a variable plot and a linear regression plot output as a .png file
     '''
     unique_values = df[groups].unique()
     # print(unique_values)
@@ -59,8 +115,8 @@ def plot_all(df, column1, column2, groups):
          
 
 if __name__ == '__main__':
-    if len(sys.argv) < 5:
-        print("Expecting file name + four arguments: .csv file, column1, column2, grouping column (e.g., column with species names)")
+    if len(sys.argv) != 5:
+        print("Expecting python file name + four arguments: .csv file, column1, column2, grouping column (e.g., column with species names). You provided", len(sys.argv)-1, "arguments.")
     else:
         data = sys.argv[1]
         column1 = sys.argv[2]
